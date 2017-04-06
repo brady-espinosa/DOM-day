317011c22a5cdbcc7c210db5755cb92a6a993455
Alex, Brady <bradyr.espinosa@gmail.com>
2017-04-06 16:50:43 -0700

    added max speed to lambo

--- DOMcar.js
+++ DOMcar.js
@@ -3,7 +3,23 @@
 var mustang = createCar("Mustang", 1969, "Red", "mustang.png", 8);
 var prius = createCar("Prius", 2017, "blue", "prius.png", 0);
 var lambo = createCar("Lambo", 2017, "silver", "lambo.png", 20);
-var bus = createCar("VW Bus", 1960, "blue", "VW.jpg", -5 );
+var bus = createCar("VW Bus", 1960, "blue", "VW.png", -5 );
+
+lambo.accelerate = function(){
+    this.speed += (10 + this.boost)
+    if (this.speed >= 200){
+      this.speed = 200
+    }
+    console.log(this.speed)
+}
+
+mustang.addInfo("The Ford Mustang is an American automobile manufactured by Ford. It was originally based on the platform of the second generation North American Ford Falcon, a compact car. The original 1962 Ford Mustang I two-seater concept car had evolved into the 1963 Mustang II four-seater concept car which Ford used to pretest how the public would take interest in the first production Mustang. The 1963 Mustang II concept car was designed with a variation of the production model's front and rear ends with a roof that was 2.7 inches shorter.")
+
+lambo.addInfo("Automobili Lamborghini S.p.A. is an Italian brand and manufacturer of luxury sports cars and SUVs based in Sant'Agata Bolognese, Italy. The company is owned by the Volkswagen Group through its subsidiary Audi. Ferruccio Lamborghini, an Italian manufacturing magnate, founded Automobili Ferruccio Lamborghini S.p.A. in 1963 to compete with established marques, including Ferrari. The company gained wide acclaim in 1966 for the Miura sports coupé, which established rear mid-engine, rear wheel drive as the standard layout for high-performance cars of the era. Lamborghini grew rapidly during its first decade, but sales plunged in the wake of the 1973 worldwide financial downturn and the oil crisis.")
+
+bus.addInfo("The Volkswagen Type 2, known officially (depending on body type) as the Transporter, Kombi or Microbus, or, informally, as the Bus (US) or Camper (UK), is a forward control panel van introduced in 1950 by the German automaker Volkswagen as its second car model. Following – and initially deriving from Volkswagen's first model, the Type 1 (Beetle) – it was given the factory designation Type 2.As one of the forerunners of the modern cargo and passenger vans, the Type 2 gave rise to forward control competitors in the United States in the 1960s, including the Ford Econoline, the Dodge A100, and the Chevrolet Corvair 95 Corvan, the latter adopting the Type 2's rear-engine configuration.")
+
+prius.addInfo("The Toyota Prius is a full hybrid electric automobile developed by Toyota and manufactured by the company since 1997. Initially offered as a 4-door sedan, it has been produced only as a 5-door hatchback since 2003. The United States Environmental Protection Agency and California Air Resources Board (CARB) rate the Prius as among the cleanest vehicles sold in the United States based on smog-forming emissions. The 2016 model year Prius Eco ranks as the all-time most fuel efficient gasoline-powered car available in the US without plug-in capability.The Prius first went on sale in Japan in 1997, and was available at all four Toyota Japan dealerships, making it the first mass-produced hybrid vehicle. It was subsequently introduced worldwide in 2000. he Prius is sold in over 90 markets, with Japan and the United States being its largest markets.")
 
 var cars = [mustang, prius, lambo, bus]
 var index = 0
@@ -17,6 +33,7 @@ function createCar(newMake, newModel, newColor, img, boostVal) {
     speed: 0,
     image: img,
     boost: boostVal,
+    info: "",
     getData: function(){ return this.color + ", " + this.modelYear + ", " + this.make},
     getSpeed: function(){ return this.speed},
     accelerate: function(){
@@ -33,6 +50,9 @@ function createCar(newMake, newModel, newColor, img, boostVal) {
           }
           console.log(this.speed)
         },
+    addInfo: function(string){
+      this.info = string;
+    },
     brake: function(){
         this.speed -= 7
         if(this.speed < 0){
@@ -95,6 +115,8 @@ function nextCar(){
   }
   newCar = cars[index];
   document.getElementById("mainImg").src = newCar.image;
+  document.getElementById("info").innerHTML = newCar.info;
+  document.getElementById("carName").innerHTML = newCar.make;
 }
 
 
